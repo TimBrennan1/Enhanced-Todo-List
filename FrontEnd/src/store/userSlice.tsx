0ad7bd3c90ea7212c0c2af734652cadd54f78c4a
Tim Brennan <t.brennan874@gmail.com>
2023-03-31 15:51:04 -0400

    More project setup

---
+++ FrontEnd/src/store/userSlice.tsx
@@ -0,0 +1,15 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// const dummy_data = Array<number>;
+
+// const slice = createSlice({
+//   name: "user",
+//   initialState: dummy_data,
+//   reducers: {
+//     increment: (state, action: PayloadAction<number>) => state + action.payload,
+//   },
+// });
+// // now available:
+// slice.actions.increment(2);
+// // also available:
+// slice.caseReducers.increment(0, { type: "increment", payload: 5 });
